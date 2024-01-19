cf382b5b95857503a972242839c1b50f80fbcdbd
juansinmiedo249 <92175341+juansinmiedo249@users.noreply.github.com>
2024-01-18 19:08:25 -0500

    Add files via upload

---
+++ view/indexView.js
@@ -0,0 +1,13 @@
+
+
+export function color(input){
+    input.style.backgroundColor = "rgba(255, 255, 0, 0.489)";
+    
+    
+}
+
+export function revertirEstilos(input) {
+    // Restablecer estilos predeterminados
+    input.style.backgroundColor = '';
+    input.style.color = '';
+  }
